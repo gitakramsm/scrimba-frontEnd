703a579f3e43ec6e6fd38a9de05ebb2f1c639713
gitakramsm <akram5960@gmail.com>
2023-10-11 17:52:17 +0530

    module3
    css , divs , width:100%

---
+++ oldagram/index.js
@@ -0,0 +1,71 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+// const bodyEl = document.querySelector("body")
+// render(posts)
+
+// function render(users){
+//     for(let i = 0; i < users.length; i++){
+//         bodyEl.innerHTML += `
+//             <section>
+//                 <div class="container align-center">
+//                     <img class="avatar" src="${users[i].avatar}">
+//                     <p class="user-name-adress">
+//                         <span class="user-name">${users[i].name}</span>
+//                         <br>
+//                         ${users[i].location}
+//                     </p>
+//                 </div>
+//             </section>
+            
+//             <section>
+//                 <div class="container">
+//                     <img class="post-img" src="${users[i].post}">
+//                 </div>
+//             </section>
+            
+//             <section>
+//                 <div class="container">
+//                     <div class="icon-wrapper">
+//                         <img class="like icon-size" src="images/icon-heart.png">
+//                         <img class="comment icon-size" src="images/icon-comment.png">
+//                         <img class="dm icon-size" src="images/icon-dm.png">
+//                     </div>
+//                     <p><span class="likes">${users[i].likes}</span></p>
+//                     <p>
+//                         <span class="user-id">${users[i].username}</span> 
+//                         ${users[i].comment}
+//                     </p>
+//                 </div>    
+//             </section>
+//         `
+//     }
+// }
+
